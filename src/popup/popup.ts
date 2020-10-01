85fc95f28dbeca0e2fd76f36cb558c3497ee6470
Alex McLean <alex.mclean95@gmail.com>
2020-10-01 11:58:44 +1000

    Popup theme

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,11 +1,21 @@
+import { DEFAULT_THEME } from '../background/defaultSettings';
+import { SETTINGS_THEME_KEY } from '../options/settings';
 import { createAggregate } from './aggregate';
 import { constructContentSettings } from './contentSettings';
 import { constructCookies } from './cookies';
 import { createPisSection } from './pisSection';
 import { constructWebRequest } from './webRequest';
 
-createAggregate();
-constructContentSettings();
-constructWebRequest();
-constructCookies();
-createPisSection();
+chrome.storage.local.get(SETTINGS_THEME_KEY, (items) => {
+  const theme = items[SETTINGS_THEME_KEY] ?? DEFAULT_THEME;
+
+  const body = document.getElementById('body');
+  if (!body) return;
+  body.className = theme;
+
+  createAggregate();
+  constructContentSettings();
+  constructWebRequest();
+  constructCookies();
+  createPisSection();
+});
