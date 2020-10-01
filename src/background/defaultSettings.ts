cdeb4269ae1e39b1c561e86585bdfd3e50215d24
Alex McLean <alex.mclean95@gmail.com>
2020-10-01 12:35:44 +1000

    Removed old vendor stuff

--- src/background/defaultSettings.ts
+++ src/background/defaultSettings.ts
@@ -2,5 +2,5 @@ export const DEFAULT_ALLOW_LIST = [];
 export const DEFAULT_BLOCK_LIST = [];
 export const HARDCODED_BLOCK_LIST = ['youtube.com'];
 
-export const DEFAULT_THEME = 'dark';
+export const DEFAULT_THEME = 'light';
 export const THEMES = ['light', 'dark', 'hulkamania'];
